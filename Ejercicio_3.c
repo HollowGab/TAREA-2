7b58da72917cd54b84383cd6a4942bacea643cc5
HollowGab <gabri.areani411@gmail.com>
2025-05-07 14:39:10 -0600

    Update Ejercicio_3.c

--- Ejercicio_3.c
+++ Ejercicio_3.c
@@ -1,2 +1,14 @@
 // Arellanes Aniceto Gabriel
-// Ejercicio_2: Calculadora del area y circunferencia de un circulo
+// Ejercicio_3: Casa de Cambio
+#include <stdio.h>
+#define Dolar 11.96
+int main()
+{
+    float D,P;
+
+    printf("¿Cuantos dolares quieres cambiar? \n");
+    scanf("%f",&D);
+    P= Dolar*D;
+     printf("%.2f dolares equivalen a %.2f pesos \n",D,P);
+    return 0;
+}
