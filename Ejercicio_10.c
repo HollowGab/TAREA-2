217addee55977a83bbbaefb6e8d764b1c8fbaf6a
HollowGab <gabri.areani411@gmail.com>
2025-05-12 14:40:25 -0600

    Create Ejercicio_10.c

---
+++ Ejercicio_10.c
@@ -0,0 +1,34 @@
+//Arellnaes Aniceto Gabriel
+//Ejercicio_10: 
+#include <stdio.h>
+
+int main() 
+{
+    int i, num;
+    int positivos = 0, negativos = 0, nulos = 0;
+
+    for (i = 0; i < 10; i++) 
+    {
+        printf("Ingrese el número %d: ", i + 1);
+        scanf("%d", &num);
+
+        if (num > 0) 
+        {
+            positivos++;
+        } 
+        else if (num < 0) 
+        {
+            negativos++;
+        } 
+        else 
+        {
+            nulos++;
+        }
+    }
+
+    printf("Cantidad de números positivos: %d \n", positivos);
+    printf("Cantidad de números negativos: %d \n", negativos);
+    printf("Cantidad de números nulos: %d \n", nulos);
+
+    return 0;
+}
