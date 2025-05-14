fff0afddef648d3b6b7d774ced009852c87fec2f
HollowGab <gabri.areani411@gmail.com>
2025-05-14 12:40:46 -0600

    Update Ejercicio_12.c

--- Ejercicio_12.c
+++ Ejercicio_12.c
@@ -1,19 +1,22 @@
 //Arellanes Aniceto Gabriel
 //Ejercicio_12: producto de los N primeros numeros naturales.
 #include <stdio.h>
-int main() 
-{
-    int N, producto = 1, i;
+int main() {
+    int N;
+    int producto = 1;
+    int i = 1;
 
-    printf("¿Cuantos de los primeros numeros naturales quieres multiplicar?\n ");
+    printf("¿Cuantos de los primeros numeros naturales quieres multiplicar? \n ");
     scanf("%d", &N);
-
-        for (i = 1; i <= N; i++) 
-        {
-            producto *= i;
-        }
-        printf("El producto de los %d primeros numeros naturales es: %d \n", N, producto);
     
+    while (i <= N) 
+    {
+            producto *= i;
+            i++;
+    }
+    
+    printf("El producto de los %d primeros numeros naturales es: %d \n", N, producto);
+
 
     return 0;
 }
