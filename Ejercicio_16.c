3332fdcea8b3931a4f362ac1b708b136a75d2212
HollowGab <gabri.areani411@gmail.com>
2025-05-14 14:30:22 -0600

    Create Ejercicio_16.c

---
+++ Ejercicio_16.c
@@ -0,0 +1,30 @@
+//Arellanes Aniceto Gabriel 
+// Ejercicio_16: Primero 100 numeros pares
+include <stdio.h>
+void main() 
+{
+    int P[100];
+    int i = 0;
+    int num = 1;
+
+    
+    while (i < 100) 
+    {
+        if (num % 2 == 0) 
+        {
+            P[i] = num;
+            i++;
+        }
+        num++;
+    }
+
+    printf("Los primeros 100 números pares son:\n");
+    i = 0;
+    while (i < 100) {
+        printf(", %d", P[i]);
+        i++;
+    }
+
+    printf("\n");
+    
+}
